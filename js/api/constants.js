1ccdddd1db8bc102865a4a7f1986ec5199220c8b
Pawwol1 <wolski.pawel1@gmail.com>
2022-03-28 13:10:52 +0200

    most of functionality added

---
+++ js/api/constants.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://todo-api.coderslab.pl/api";
+export const API_KEY = "2f1db962-1d73-408b-9d55-0fe6f71e972c";
+
+// export const API_URL = "http://localhost/api";
+// export const API_KEY = "090132cc-3eb0-497a-b707-fba5e5fb4f00";
+
